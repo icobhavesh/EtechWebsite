0a909b30b21cc572ba2b1806b883e991ff06a586
icobhavesh <bhaveshsaxena058@gmail.com>
2023-11-25 13:40:47 +0530

    Update in vite.config.js package.json package-lock-json  with changes

--- vite.config.js
+++ vite.config.js
@@ -29,15 +29,23 @@
 //   },
 // });
 
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     rollupOptions: {
+//       external: ['react-typed'],
+//     },
+//   },
+// });
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import checker from 'vite-plugin-checker';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['react-typed'],
-    },
-  },
+  plugins: [react(), checker({ typescript: true })],
 });
