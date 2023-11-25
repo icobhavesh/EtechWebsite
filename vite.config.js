0027330e407d10ff0ae22c212b20d08b198161a4
icobhavesh <bhaveshsaxena058@gmail.com>
2023-11-25 13:34:03 +0530

    Update in vite.config.js  with changes

--- vite.config.js
+++ vite.config.js
@@ -16,6 +16,19 @@
 //     include: ['react-icons/ai'],
 //   },
 // });
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     rollupOptions: {
+//       external: ['react-icons/ai'],
+//     },
+//   },
+// });
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -24,7 +37,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['react-icons/ai'],
+      external: ['react-typed'],
     },
   },
 });
